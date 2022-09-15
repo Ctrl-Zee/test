433e5d63d546e6de40824c3b1337092c0a508e5a
andrew_smith_moser <andrew.smith@moserit.com>
2022-09-15 16:06:23 -0400

    initial ui commit

---
+++ intern-library-management-ui/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.module').then((m) => m.BooksModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
